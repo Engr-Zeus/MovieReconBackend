589b2d00b8305d28174f68ef0527cf455f394401
Engr Zeus Kingsley <engrkingsley@live.com>
2025-07-30 15:12:17 +0100

    cors

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const corsOptions = {
+  origin: 'https://movie-recon-frontend-3dxmdoc61-engr-zeus-projects.vercel.app',
+  credentials: true,
+};
+
 // Import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
@@ -12,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
